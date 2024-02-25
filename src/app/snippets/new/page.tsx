68b2a1c7667665be8f9786cd2320ad9783d3dacd
freddy24-7 <freddy.austli@aol.com>
2024-02-25 15:01:11 +0100

    adding to git

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from 'next/navigation';
-import { db } from '@/db';
+import { db } from '../../../db';
 
 export default function SnippetCreatePage() {
   async function createSnippet(formData: FormData) {
